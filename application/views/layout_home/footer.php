2ac5058f47221b18f31755f2fdc9f0a6f3d51645
faridangga <faridangga@polinema.ac.id>
2020-10-22 20:57:04 +0700

    footer

--- application/views/layout_home/footer.php
+++ application/views/layout_home/footer.php
@@ -27,20 +27,16 @@
                            
                         </ul>
                     </div><!-- .col -->
-
-                    
-
                     <div class="col-12 col-md-6 col-lg-5 mt-5 mt-md-0">
                         <div class="foot-contact">
                             <h2>Contact</h2>
-
 
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22351.917547062232!2d112.63128660429628!3d-7.96811118435145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd62822063dc2fb%3A0x78879446481a4da2!2sMalang%2C%20Kota%20Malang%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1589296580919!5m2!1sid!2sid" width="350" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
                             <ul>
                                 <li><i class="fa fa-phone"></i><span>+62 1234 456 789</span></li>
-                                <li><i class="fa fa-envelope"></i><span>charity@gmail.com</span></li>
-                                <li><i class="fa fa-map-marker"></i><span>Gang Kelapa Sumbersuko Lawang</span></li>
+                                <li><i class="fa fa-envelope"></i><span>CMS</span></li>
+                                <li><i class="fa fa-map-marker"></i><span>Malang</span></li>
                             </ul>
                         </div><!-- .foot-contact -->
 
